c18b0c388ddcce914428a1188c34e0e37909ce05
dbaker3 <dbaker3@gmail.com>
2015-06-25 13:07:52 -0400

    Script changes menu on front page

---
+++ scripts/welshimer.js
@@ -0,0 +1,6 @@
+if (document.getElementById('sidemenu')) {
+    $("#ds-search-option").appendTo("#file_news_div_news");
+    $('#ds-search-option').css('max-width', '400px');
+    $('#sidebar').empty(); 
+    $('#sidemenu').appendTo('#sidebar');
+}
