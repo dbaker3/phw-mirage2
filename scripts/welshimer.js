feab607bafa06d3d8db40900bd24f3a602c6b86c
dbaker3 <dbaker3@gmail.com>
2015-08-03 16:38:22 -0400

    Logo change, homepage menu placement

--- scripts/welshimer.js
+++ scripts/welshimer.js
@@ -1,7 +1,28 @@
-if (document.getElementById('sidemenu')) {
+/*if (document.getElementById('sidemenu')) {
     $("#ds-search-option").appendTo("#file_news_div_news");
     $('#ds-search-option').css('max-width', '400px');
     $('#ds-options').empty(); 
     $('#sidemenu').appendTo('#ds-options');
     $('#sidebar').prependTo('.horizontal-slider');
+}*/
+
+// sidemenu.html resides in [source]\dspace\modules\xmlui\src\main\webapp\static\
+
+// MCStor homepage
+if (window.location.pathname == "/") {
+    $.get('/sidemenu.html', function(sidemenu) {
+        $(sidemenu).appendTo('#sidebar');
+        $('#ds-search-option').appendTo('#file_news_div_news');
+        $('#ds-search-option').css('max-width', '400px');
+    });
+    $('#ds-options').hide();
+}
+
+// Pages defined in xsl/core/page-structure.xsl
+if (window.location.pathname.substring(0,5) == "/page") {
+    $.get('/sidemenu.html', function(sidemenu) {
+        $(sidemenu).appendTo('#sidebar');
+        $('#ds-search-option').prependTo('#sidemenu');
+    });
+    $('#ds-options').hide();
 }
